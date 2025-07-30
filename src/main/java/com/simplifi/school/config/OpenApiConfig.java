972f63a364467cb6350980467b70161aaa47e055
Alina Saghir <Alinasaghir@users.noreply.github.com>
2025-07-30 15:27:21 +0500

    feat(config): add OpenAPI 3 configuration for Swagger UI

---
+++ src/main/java/com/simplifi/school/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.simplifi.school.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI schoolManagementOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("School Management API")
+                        .description("Spring Boot application for School Management System")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
